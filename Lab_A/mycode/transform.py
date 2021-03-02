4dfde06b611a6dd89a59336087356d394183036a
Semme Dijkstra <semmed@ccom.unh.edu>
2021-03-02 17:45:35 +0000

    before Lab D step 3

---
+++ Lab_A/mycode/transform.py
@@ -0,0 +1,34 @@
+import numpy as np
+from numpy import cos, sin
+
+def Rx( angle = 0):
+    R = ...
+
+    return R
+
+def Px():
+    P = ...
+    return P
+
+def Ry( angle = 0):
+    pass
+
+def Py():
+    pass
+
+def Rz( angle = 0):
+    pass
+
+def Pz():
+    pass
+
+def swap_xy( vec):
+    if vec.shape == (3,):
+        return ...
+    if vec.shape == (1,3):
+        return ...
+    if vec.shape == (3,1):
+        return ...
+
+
+        
